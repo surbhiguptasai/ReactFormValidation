c6414b39fda3d12b804c2a97dc023e8fdf8cb45a
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2018-10-25 23:12:12 -0400

    background color

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import FieldLevelValidationForm from './FieldLevelValidationForm'
 // import { Field, reduxForm } from 'redux-form'
-
+import showResults from './shiowResults'
+import { Values } from 'redux-form-website-template'
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1 className="heading-center">Form Validations</h1>
-        <FieldLevelValidationForm />
+      // <div>
+      //   <h1 className="heading-center">Form Validations</h1>
+      //   <FieldLevelValidationForm onSubmit={showResults} />
+
+      // </div>
+      <div className="formvalues">
+        <div className=" col-xs-12 col-md-8">
+          <h2>Field-Level Validation</h2>
+          <FieldLevelValidationForm onSubmit={showResults} />
+        </div>
+        <div className="col-xs-12 col-md-3 w3-card w3-pink val">
+          <Values className="" form="fieldLevelValidation" />
+        </div>
       </div>
     )
   }
